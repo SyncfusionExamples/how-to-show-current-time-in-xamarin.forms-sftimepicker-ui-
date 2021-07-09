e92e8403869ec58f3ba08fc5f634646e620685c2
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-07-09 20:59:16 +0530

    TimePicker sample has been added

---
+++ TimePickerSample/TimePickerViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TimePickerSample
+{
+    class TimePickerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private TimeSpan selectedTime = new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+
+        public TimeSpan SelectedTime
+        {
+            get { return selectedTime; }
+            set
+            {
+                selectedTime = value;
+                NotifyPropertyChanged("SelectedTime");
+            }
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
